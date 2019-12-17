81027ed90b4e47346f9ece88383b6603b4688589
Adam Weissman <adam@comedytragedyepic.com>
2019-12-17 11:39:51 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,11 +1,12 @@
 class Dog
   attr_accessor :name, :breed, :age
   
-  @all_dogs = []
+  @@all_dogs = []
   
   def initialize(name, breed, age)
     @name = name
     @breed = breed
     @age = age
+    @@all_dogs << self
   end
 end
